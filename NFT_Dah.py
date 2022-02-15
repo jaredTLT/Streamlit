0e3a57d8a3ea88c08c7b6e5065de856957a5e69c
jaredTLT <53750434+jaredTLT@users.noreply.github.com>
2022-02-15 15:13:33 +0200

    Update NFT_Dah.py

--- NFT_Dah.py
+++ NFT_Dah.py
@@ -46,7 +46,31 @@ def main():
                             "filesize":data_file.size}
 			
         st.write(file_details)
+	def Getrows():
+    arr=[]
+    for row in cursor.tables():
+        #print(row[2])
+        if 'T7' in row[2]:
+            arr.append(row[2])
+    return arr
 
+def getFirstCol(name):
+    return cursor.columns(table=name).fetchall()[0].column_name
+
+     
+def getLastCol(name):
+    return cursor.columns(table=name).fetchall()[-1].column_name
+    
+    
+def getVuserTable():
+    for row in Getrows(): 
+        if getLastCol(row) =="Event Name" and getFirstCol(row)=='Minimum':
+            print(f'SELECT * FROM {row}')
+            return cursor.execute(f'SELECT*FROM {row}')
+	
+conn=pyodbc.connect(data_file)
+cursor = conn.cursor()
+getVuserTable()
 
 if __name__ == '__main__':
 	main()
