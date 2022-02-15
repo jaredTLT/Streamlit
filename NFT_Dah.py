d439e57c2ff7237d22d26423ded8da8e41ea4ccf
jaredTLT <53750434+jaredTLT@users.noreply.github.com>
2022-02-15 15:23:38 +0200

    Update NFT_Dah.py

--- NFT_Dah.py
+++ NFT_Dah.py
@@ -14,8 +14,7 @@ def main():
 	def Getrows():
  		arr=[]
 		for row in cursor.tables():
-        	#print(row[2])
-       			if 'T7' in row[2]:
+			if 'T7' in row[2]:
             			arr.append(row[2])
     				return arr
 	def getFirstCol(name):
